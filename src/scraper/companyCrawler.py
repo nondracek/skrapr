3b775749170687739d29768968e6c13ca08f63ab
dastjerdi <dastjerdi@college.harvard.edu>
2018-06-05 12:42:06 -0400

    Class based

--- src/scraper/companyCrawler.py
+++ src/scraper/companyCrawler.py
@@ -6,50 +6,64 @@ from urllib.parse import urlencode
 from urllib.parse import quote
 
 
-company = {}
-company['keywords'] = input("Search?")
-baseURL = 'https://www.linkedin.com/search/results/companies/?'
-# append query to URL in correct encoding
-companySearchURL = baseURL + urlencode(company, quote_via=quote)
+class companySearch:
+    def __init__(self):
+        self.chromedriverPath = os.getcwd() + '/dependencies/chromedriver'
+        self.company = {}
+        self.baseURL ='https://www.linkedin.com/search/results/companies/?'
+        self.results = []
+    
+    def connect(self, cookie):
+        self.browser = webdriver.Chrome(self.chromedriverPath)
+        self.browser.get('https://www.linkedin.com')
+        self.cookie = cookie
+        self.browser.add_cookie({
+            'name': 'li_at',
+            'value': self.cookie,
+            'domain': '.linkedin.com'
+        })
 
-# all old code for navigation
-###########################
-# Set the path of the chromedriver
-chromedriverPath = os.getcwd() + '/dependencies/chromedriver'
+    def search(self, input_company = None):
+        # CLI search
+        if not input_company:
+            self.company['keywords'] = input("Search?")
+        else: 
+            self.company= input_company
+        # construct URL and scrape results
+        companySearchURL = self.baseURL + urlencode(self.company, quote_via=quote)
+        self.browser.get(companySearchURL)
+        html = self.browser.page_source
+        self.soup = BeautifulSoup(html, 'lxml')
 
-# Initiate the webdriver and set the li_at cookie
-browser = webdriver.Chrome(chromedriverPath)
-browser.get('https://www.linkedin.com')
-cookie = 'AQEDARz6R7AAO180AAABY8dfBtIAAAFj62uK0lYAz8QXn6gzeD8nhfHd3MTNmHrdIni1EV51KoSTXtdcf9yiY1XppYYa-DISIY3cusw_ieSBVPvumyU-yVLe42EQbdFEBfXIRbdRL8sdxl-0jAoie8ua'
-browser.add_cookie({
-    'name': 'li_at',
-    'value': cookie,
-    'domain': '.linkedin.com'
-})
-###########################
+    def get_results(self, n = None, ):
+        companyResults = self.soup.find_all("div", {"class": "search-result__info"})
+        # for each company
+        for companyResult in companyResults: 
+            # get their name
+            name = companyResult.find('a', {"class": "search-result__result-link"}).text.lstrip().rstrip()
+            # and the URL to their profile  
+            liURL = companyResult.find('a', {"class": "search-result__result-link"}).attrs['href']
+            URL = 'https://www.linkedin.com' + liURL
+            # Get their industry
+            industry = companyResult.find('p', {'class': "subline-level-1"}).text.lstrip().rstrip()
+            self.results += [{'name': name, 'URL': URL, 'industry': industry}]
+        # CLI output
+        if not n: 
+            n = int(input('# of results?'))
+            print(self.results[:n])
+        return self.results[:n]
 
-# go to search URL
-# Retrieve html in xml format from a url
-browser.get(companySearchURL)
-html = browser.page_source
-soup = BeautifulSoup(html, 'lxml')
+crawler =companySearch()
+crawler.connect('AQEDARz6R7AAO180AAABY8dfBtIAAAFj62uK0lYAz8QXn6gzeD8nhfHd3MTNmHrdIni1EV51KoSTXtdcf9yiY1XppYYa-DISIY3cusw_ieSBVPvumyU-yVLe42EQbdFEBfXIRbdRL8sdxl-0jAoie8ua')
+crawler.search()
+crawler.get_results()
 
-# empty list for search result URLs
-results = []
-# find people in results
-companyResults = soup.find_all("div", {"class": "search-result__info"})
-# for each person
-for companyResult in companyResults: 
-    # get their name
-    name = companyResult.find('a', {"class": "search-result__result-link"}).text.lstrip().rstrip()
-    # and the URL to their profile  
-    liURL = companyResult.find('a', {"class": "search-result__result-link"}).attrs['href']
-    URL = 'https://www.linkedin.com' + liURL
-    # Get their industry
-    industry = companyResult.find('p', {'class': "subline-level-1"}).text.lstrip().rstrip()
-    results += [{'name': name, 'URL': URL, 'industry': industry}]
 
-n = int(input('# of results?'))
 
-print(results[:n])
+
+
+
+
+
+
 
--- src/scraper/personCrawler.py
+++ src/scraper/personCrawler.py
@@ -5,55 +5,67 @@ from selenium import webdriver
 from urllib.parse import urlencode
 from urllib.parse import quote
 
-# empty dictionary of person query info
-person = {}
-# list of query parameters
-personQueries = ['firstName','lastName', 'company', 'title', 'school']
-# get parameters from user
-for query in personQueries:
-    value = input(query + '=')
-    if value: 
-        person[query] = value
-baseURL = 'https://www.linkedin.com/search/results/people/?'
-# append query to URL in correct encoding
-personSearchURL = baseURL + urlencode(person, quote_via=quote)
 
-# all old code for navigation
-###########################
-# Set the path of the chromedriver
-chromedriverPath = os.getcwd() + '/dependencies/chromedriver'
+class personSearch:
+    def __init__(self):
+        self.chromedriverPath = os.getcwd() + '/dependencies/chromedriver'
+        self.person = {}
+        self.personQueries = ['firstName','lastName', 'company', 'title', 'school']
+        self.baseURL = 'https://www.linkedin.com/search/results/people/?'
+        self.results = []
+    
+    def connect(self, cookie):
+        self.browser = webdriver.Chrome(self.chromedriverPath)
+        self.browser.get('https://www.linkedin.com')
+        self.cookie = cookie
+        self.browser.add_cookie({
+            'name': 'li_at',
+            'value': self.cookie,
+            'domain': '.linkedin.com'
+        })
 
-# Initiate the webdriver and set the li_at cookie
-browser = webdriver.Chrome(chromedriverPath)
-browser.get('https://www.linkedin.com')
-cookie = 'AQEDARz6R7AAO180AAABY8dfBtIAAAFj62uK0lYAz8QXn6gzeD8nhfHd3MTNmHrdIni1EV51KoSTXtdcf9yiY1XppYYa-DISIY3cusw_ieSBVPvumyU-yVLe42EQbdFEBfXIRbdRL8sdxl-0jAoie8ua'
-browser.add_cookie({
-    'name': 'li_at',
-    'value': cookie,
-    'domain': '.linkedin.com'
-})
-###########################
+    def search(self, input_person = None):
+        # CLI search
+        if not input_person:
+            for query in self.personQueries:
+                value = input(query + '=')
+                if value: 
+                    self.person[query] = value
+        else: 
+            self.person = input_person
 
-# go to search URL
-# Retrieve html in xml format from a url
-browser.get(personSearchURL)
-html = browser.page_source
-soup = BeautifulSoup(html, 'lxml')
+        # construct URL and scrape results
+        personSearchURL = self.baseURL + urlencode(self.person, quote_via=quote)
+        self.browser.get(personSearchURL)
+        html = self.browser.page_source
+        self.soup = BeautifulSoup(html, 'lxml')
 
-# empty list for search result URLs
-results = []
-# find people in results
-personResults = soup.find_all("span", {"class": "name actor-name"})
-# for each person
-for personResult in personResults: 
-    # get their name
-    name = personResult.text
-    # and the URL to their profile  
-    liURL = personResult.parent.parent.parent.attrs['href']
-    URL = 'https://www.linkedin.com' + liURL
-    results += [{'name': name, 'URL': URL}]
+    def get_results(self, n = None, ):
+        personResults = self.soup.find_all("span", {"class": "name actor-name"})
+        # for each person
+        for personResult in personResults: 
+            # get their name
+            name = personResult.text
+            # and the URL to their profile  
+            liURL = personResult.parent.parent.parent.attrs['href']
+            URL = 'https://www.linkedin.com' + liURL
+            self.results += [{'name': name, 'URL': URL}]
+        # CLI output
+        if not n: 
+            n = int(input('# of results?'))
+            print(self.results[:n])
+        return self.results[:n]
 
-n = int(input('# of results?'))
+crawler = personSearch()
+crawler.connect('AQEDARz6R7AAO180AAABY8dfBtIAAAFj62uK0lYAz8QXn6gzeD8nhfHd3MTNmHrdIni1EV51KoSTXtdcf9yiY1XppYYa-DISIY3cusw_ieSBVPvumyU-yVLe42EQbdFEBfXIRbdRL8sdxl-0jAoie8ua')
+crawler.search()
+crawler.get_results()
 
-print(results[:n])
+
+
+
+
+
+
+
 
