25bb3e9f918906ae318b33b7981aaf1f27665e7a
dastjerdi <dastjerdi@college.harvard.edu>
2018-06-15 11:41:37 -0400

    minor change

--- src/websiteRetriever/logIn.py
+++ src/websiteRetriever/logIn.py
@@ -25,7 +25,6 @@ class logIn():
         }
         
         client.post(LOGIN_URL, data=login_information)
-        print(client.cookies)
         
         self.cookie = client.cookies['li_at']
 
